e6995067586ae814db57bd811d7f0b10d310f906
eltonbarbosa <eltonbr.barbosa@gmail.com>
2016-10-01 15:43:03 -0300

    Exemplo App cordova

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,8 @@
 var App = {
-    
-    BASE_URL : 'http://tmebr.esy.es/apis/cardapio/',
 
     init : function(){
-
         //exibe o cardapio humburger incialmente
-        App.getCardapio('humburgers');
-
+        $('#cardapio-humburger').show();
         //No evento onclick de alguma opção 
         $('.opcoes').click(function(){
 
@@ -15,41 +11,14 @@ var App = {
 
             if($(this).attr('id') === 'humburgers'){//exibe o menu humburgers e marca como selecionado
                 $('#label-humburger').addClass('selecionado');
-                App.getCardapio('humburgers');
+                $('#cardapio-coffe').hide();
+                $('#cardapio-humburger').show();
             }else{//exibe o menu coffe e marca como selecionado
                 $('#label-coffe').addClass('selecionado');
-                App.getCardapio('coffes');
+                $('#cardapio-humburger').hide();
+                $('#cardapio-coffe').show();
             }
         });
-    },
-
-    getCardapio : function (categoria){
-        $.ajax({
-            url: App.BASE_URL + categoria,
-            error: function (erro) {
-               console.log('Erro ao acessar a lista:' + categoria + ' - ' + erro);
-            },
-            success: function (dados) {
-                App.construirCardapio(dados);
-            }
-        });
-    },
-
-    construirCardapio : function(dados){
-
-        if(dados && dados.length > 0){
-            $( ".cardapio" ).empty();
-            var opcoes = JSON.parse(dados);
-            for(var i = 0; i < opcoes.length; i++){
-                $( ".cardapio" ).append(
-                    "<li class='items'>" +  
-                        "<span class='items-nome'>" + opcoes[i].nome + "</span>" +
-                        "<span class='items-descricao'>" + opcoes[i].descricao + "</span>" +
-                        "<span class='items-preco'>" + "R$" + opcoes[i].preco + "<span>" + 
-                    "</li>"
-                );
-            }
-        }
     }
 
 }
